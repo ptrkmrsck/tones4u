a8e446521826b2cae1616045a205de8111f295dd
ptrk <patrick.m.marschke@gmail.com>
2023-01-14 11:35:06 -0600

    updating to 1.0

--- src/routes/api.json/+server.js
+++ src/routes/api.json/+server.js
@@ -1,20 +1,21 @@
+import { json as json$1 } from '@sveltejs/kit';
 import db from '$lib/db.js';
 
-export async function get() {
+export async function GET() {
 	const { data, error } = await db
 		.from('tones4uArchive')
 		.select('*')
 		.eq('approved', true)
 		.order('created_at', { ascending: false });
 
-	return { status: 200, body: { data, error } };
+	return json$1({ data, error });
 }
 
-export async function post({ request }) {
+export async function POST({ request }) {
 	const formData = await request.json();
 	const { description, url } = await formData;
 
 	const { data, error } = await db.from('tones4uArchive').insert([{ description, url }]);
 
-	return { status: 201, data, error };
+	return new Response(undefined, { status: 201 });
 }
